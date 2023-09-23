7636eb839110fba0b495239066a99da81b0c0cc0
sina <sina_od@yahoo.com>
2023-09-23 17:50:50 +0330

    - Added `NEXT_PUBLIC_VALIDATE_HASH` - Log webapp's init data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,41 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import MainContainer from '@/components/Containers/MainContainer';
+import axios from 'axios';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
     const [isHashValid, setIsHashValid] = useState(false);
 
     // Wait for validation to complete before rendering the page and stop the
-    // rendering if the hash is invalid. Comment out the following useEffect
-    // hook to see the page render without the hash validation.
-    // useEffect(() => {
-    //   axios
-    //     .post('/api/validate-hash', { hash: window.Telegram.WebApp.initData })
-    //     .then((response: any) => setIsHashValid(response.status === 200));
-    // }, []);
+    // rendering if the hash is invalid.
+    useEffect(() => {
+        console.log({ webAppInitData: window.Telegram.WebApp.initData });
+        if (process.env.NEXT_PUBLIC_VALIDATE_HASH === 'true') {
+            axios
+                .post('/api/validate-hash', { hash: window.Telegram.WebApp.initData })
+                .then((response: any) => setIsHashValid(response.status === 200));
+        } else {
+            setIsHashValid(true);
+        }
+    }, []);
 
     if (!isHashValid) {
         return null;
     }
 
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MainContainer>
+                <Component {...pageProps} />
+            </MainContainer>
+        </>
+    );
 }
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
-import MainContainer from '@/components/Containers/MainContainer';
 
 function MyDocument() {
     return (
@@ -9,10 +8,8 @@ function MyDocument() {
                 <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
             </Head>
             <body>
-                <MainContainer>
-                    <Main />
-                    <NextScript />
-                </MainContainer>
+                <Main />
+                <NextScript />
             </body>
         </Html>
     );
