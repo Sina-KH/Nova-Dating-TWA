8a334f59e467c27fae3f7dfe4e4f24747a5356e6
sina <sina_od@yahoo.com>
2023-10-09 17:54:57 +0330

    v0.10.3: - Like/Dislike buttons in explore

--- src/components/Containers/MySwipeContainer/MySwipeContainer.tsx
+++ src/components/Containers/MySwipeContainer/MySwipeContainer.tsx
@@ -74,7 +74,7 @@ export default function MySwipeContainer({ users, userLiked, userDisliked, loadM
 
     return (
         <div
-            className={'flex flex-col w-full h-full overflow-x-hidden'}
+            className={'relative flex flex-col w-full h-full overflow-x-hidden'}
             onTouchStart={handleTouchStart}
             onTouchMove={handleTouchMove}
         >
@@ -94,7 +94,60 @@ export default function MySwipeContainer({ users, userLiked, userDisliked, loadM
                     );
                 })}
             </div>
-            {/*<MySwipeContainerActions />*/}
+            {/*Dislike hover view*/}
+            <div
+                className={'absolute top-32 left-0 drop-shadow-2xl'}
+                style={{
+                    zIndex: '9999',
+                    opacity: -translateX / 100,
+                    transform: `translateX(${-translateX / 3}px) rotate(${rotation}deg)`,
+                    transition: `transform 0.1s ease`
+                }}
+            >
+                <svg width="114" height="114" viewBox="0 0 57 57" fill="none">
+                    <circle cx="28.957" cy="28.9167" r="28" fill="white" />
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M23.0386 22.9983C23.4436 22.5933 24.1002 22.5933 24.5052 22.9983L28.9571 27.4502L33.409 22.9983C33.814 22.5933 34.4706 22.5933 34.8756 22.9983C35.2806 23.4033 35.2806 24.0599 34.8756 24.4649L30.4237 28.9168L34.8756 33.3687C35.2806 33.7737 35.2806 34.4303 34.8756 34.8353C34.4706 35.2403 33.814 35.2403 33.409 34.8353L28.9571 30.3834L24.5052 34.8353C24.1002 35.2403 23.4436 35.2403 23.0386 34.8353C22.6336 34.4303 22.6336 33.7737 23.0386 33.3687L27.4905 28.9168L23.0386 24.4649C22.6336 24.0599 22.6336 23.4033 23.0386 22.9983Z"
+                        fill="#0E1621"
+                        stroke="#0E1621"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </svg>
+            </div>
+            {/*Like hover view*/}
+            <div
+                className={'absolute top-32 right-0 drop-shadow-2xl'}
+                style={{
+                    zIndex: '9999',
+                    opacity: translateX / 100,
+                    transform: `translateX(${-translateX / 3}px) rotate(${-rotation}deg)`,
+                    transition: `transform 0.1s ease`
+                }}
+            >
+                <svg width="114" height="112" viewBox="0 0 57 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="28.5" cy="28" r="28" fill="white" className={'fill-white'} />
+                    <path
+                        d="M23.8333 19.7036C20.6833 19.7036 18.1296 22.2573 18.1296 25.4073C18.1296 31.111 24.8704 36.2962 28.5 37.5024C32.1296 36.2962 38.8704 31.111 38.8704 25.4073C38.8704 22.2573 36.3167 19.7036 33.1667 19.7036C31.2376 19.7036 29.5322 20.6613 28.5 22.1271C27.4678 20.6613 25.7624 19.7036 23.8333 19.7036Z"
+                        fill="#CC4444"
+                        stroke="#CC4444"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </svg>
+            </div>
+            <MySwipeContainerActions
+                onLike={() => {
+                    handleSwipe('right');
+                }}
+                onDislike={() => {
+                    handleSwipe('left');
+                }}
+            />
         </div>
     );
 }
--- src/components/Containers/MySwipeContainer/MySwipeContainerActions.tsx
+++ src/components/Containers/MySwipeContainer/MySwipeContainerActions.tsx
@@ -1,37 +1,48 @@
 import React from 'react';
 
-export default function MySwipeContainerActions() {
+interface Props {
+    onLike: () => void;
+    onDislike: () => void;
+}
+export default function MySwipeContainerActions({ onLike, onDislike }: Props) {
     return (
-        <div className={'absolute bottom-0 left-auto right-auto flex flex-row'}>
-            <svg width="84" height="89" viewBox="0 0 168 178" fill="none">
-                <g filter="url(#filter0_d_309_5432)">
-                    <circle cx="79" cy="69" r="39" fill="white" />
-                </g>
-                <defs>
-                    <filter
-                        id="filter0_d_309_5432"
-                        x="-10"
-                        y="0"
-                        width="178"
-                        height="178"
-                        filterUnits="userSpaceOnUse"
-                        colorInterpolationFilters="sRGB"
-                    >
-                        <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                        <feColorMatrix
-                            in="SourceAlpha"
-                            type="matrix"
-                            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                            result="hardAlpha"
-                        />
-                        <feOffset dy="20" />
-                        <feGaussianBlur stdDeviation="25" />
-                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.07 0" />
-                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_309_5432" />
-                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_309_5432" result="shape" />
-                    </filter>
-                </defs>
-            </svg>
+        <div
+            className={'absolute bottom-0 w-full flex flex-row justify-center pb-10 gap-6'}
+            style={{
+                zIndex: '9999'
+            }}
+        >
+            {/*Dislike*/}
+            <button className={'cursor-none drop-shadow-2xl'} onClick={onDislike}>
+                <svg width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="28.957" cy="28.9167" r="28" fill="white" />
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M23.0386 22.9983C23.4436 22.5933 24.1002 22.5933 24.5052 22.9983L28.9571 27.4502L33.409 22.9983C33.814 22.5933 34.4706 22.5933 34.8756 22.9983C35.2806 23.4033 35.2806 24.0599 34.8756 24.4649L30.4237 28.9168L34.8756 33.3687C35.2806 33.7737 35.2806 34.4303 34.8756 34.8353C34.4706 35.2403 33.814 35.2403 33.409 34.8353L28.9571 30.3834L24.5052 34.8353C24.1002 35.2403 23.4436 35.2403 23.0386 34.8353C22.6336 34.4303 22.6336 33.7737 23.0386 33.3687L27.4905 28.9168L23.0386 24.4649C22.6336 24.0599 22.6336 23.4033 23.0386 22.9983Z"
+                        fill="#0E1621"
+                        stroke="#0E1621"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </svg>
+            </button>
+
+            {/*Like*/}
+            <button className={'cursor-none drop-shadow-2xl'} onClick={onLike}>
+                <svg width="57" height="56" viewBox="0 0 57 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="28.5" cy="28" r="28" fill="white" className={'fill-white'} />
+                    <path
+                        d="M23.8333 19.7036C20.6833 19.7036 18.1296 22.2573 18.1296 25.4073C18.1296 31.111 24.8704 36.2962 28.5 37.5024C32.1296 36.2962 38.8704 31.111 38.8704 25.4073C38.8704 22.2573 36.3167 19.7036 33.1667 19.7036C31.2376 19.7036 29.5322 20.6613 28.5 22.1271C27.4678 20.6613 25.7624 19.7036 23.8333 19.7036Z"
+                        fill="#CC4444"
+                        stroke="#CC4444"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </svg>
+            </button>
         </div>
     );
 }
